be6405361125fa66788821064299fae4290e7b69
erick-shafikov <100657557+erick-shafikov@users.noreply.github.com>
2025-04-10 01:06:30 +0500

    fix: add theme export

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 export { ColorSchemeScript, mantineHtmlProps } from '@mantine/core'
 export { MantineProvider } from '@/components/MainProvider'
 export { Button } from '@/components/Button'
+export { theme } from '@/components/theme'
