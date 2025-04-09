f8d0ee99606c8a3505449e9271decc133394e30b
erick-shafikov <100657557+erick-shafikov@users.noreply.github.com>
2025-04-09 17:06:13 +0500

    fix: fix button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,10 @@
-import '@mantine/core/styles/Button.layer.css'
 import styles from '@/components/Button/styles.module.css'
-
 import { Button as MantineButton, ButtonProps } from '@mantine/core'
 // ----------------------------------------------------------------------
 
 export const Button = (props: ButtonProps) => (
   <MantineButton
     {...props}
-    className={styles.button}
+    classNames={styles}
   />
 )
