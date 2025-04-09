c280370cd6df0e9d40707332dad77ed77400a2d3
erick-shafikov <100657557+erick-shafikov@users.noreply.github.com>
2025-04-09 22:36:35 +0500

    fix: rollback

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,9 @@
 import { Button as MantineButton, ButtonProps } from '@mantine/core'
+import '@mantine/core/styles/UnstyledButton.css'
+import '@mantine/core/styles/Button.css'
 import styles from '@/components/Button/styles.module.css'
-// ----------------------------------------------------------------------
 
+// ----------------------------------------------------------------------
 //default props
 MantineButton.withProps({})
 
