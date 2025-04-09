af5ad814af76cd44558f906780df7445f0c47459
erick-shafikov <100657557+erick-shafikov@users.noreply.github.com>
2025-04-09 17:19:56 +0500

    fix: fix button styles

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,20 +1,14 @@
-const breakPoints = {
-  'mantine-breakpoint-xs': '36em',
-  'mantine-breakpoint-sm': '48em',
-  'mantine-breakpoint-md': '62em',
-  'mantine-breakpoint-lg': '75em',
-  'mantine-breakpoint-xl': '88em',
-}
-
-
 module.exports = {
-  plugins: [
-    require('postcss-simple-vars')({
+  plugins: {
+    'postcss-preset-mantine': {},
+    'postcss-simple-vars': {
       variables: {
-      ...breakPoints
+        'mantine-breakpoint-xs': '36em',
+        'mantine-breakpoint-sm': '48em',
+        'mantine-breakpoint-md': '62em',
+        'mantine-breakpoint-lg': '75em',
+        'mantine-breakpoint-xl': '88em',
+      },
     },
-    }), 
-    require('autoprefixer'), 
-    require('postcss-preset-mantine')
-  ], 
+  },
 }
