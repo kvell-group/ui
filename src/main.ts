4eb8dbaef91b94e2053b41203bfb35c070d22331
erick-shafikov <100657557+erick-shafikov@users.noreply.github.com>
2025-04-11 00:58:41 +0500

    fix: delete exp button

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-export { ExperimentalButtonWithSvg } from '@/components/Button/ExperimentalButtonWithSvg'
-
 export { ColorSchemeScript, mantineHtmlProps } from '@mantine/core'
 export { MantineProvider } from '@/components/MainProvider'
 export { Button } from '@/components/Button'
