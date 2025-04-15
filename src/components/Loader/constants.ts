223bc2aeb0cbafbf7af1ba1a60bd3ec7af243ee0
erick-shafikov <100657557+erick-shafikov@users.noreply.github.com>
2025-04-15 11:06:28 +0500

    fix: add loader

---
+++ src/components/Loader/constants.ts
@@ -0,0 +1,8 @@
+export const AUTO_LOADER_SIZE = 'auto'
+export const SM_LOADER_SIZE = 'sm'
+export const MD_LOADER_SIZE = 'md'
+export const LG_LOADER_SIZE = 'lg'
+
+// ----------------------------------------------------------------------
+
+export const LoaderSizes = [AUTO_LOADER_SIZE, SM_LOADER_SIZE, MD_LOADER_SIZE, LG_LOADER_SIZE]
