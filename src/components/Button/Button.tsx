2a0190b96becb0ce1cb689e928752f1de4941367
az33zy <10743275+az33zy@users.noreply.github.com>
2025-04-16 13:02:55 +0500

    fix: make button polymorphic

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,21 +1,23 @@
-import { Button as MantineButton } from '@mantine/core'
-import type { ButtonProps as BaseButtonProps, ElementProps } from '@mantine/core'
+import { createPolymorphicComponent, Button as MantineButton } from '@mantine/core'
+import type { ButtonProps as BaseButtonProps } from '@mantine/core'
 import '@mantine/core/styles/UnstyledButton.css'
 import '@mantine/core/styles/Button.css'
 import classesNames from '@/components/Button/Button.module.css'
-import { forwardRef, Ref } from 'react'
+import { forwardRef } from 'react'
 import { ExtendedButtonVariant } from '@/components/Button/types'
 
 // ----------------------------------------------------------------------
 
-type ButtonProps = BaseButtonProps & ElementProps<'button'> & { variant?: ExtendedButtonVariant }
+type ButtonProps = BaseButtonProps & { variant?: ExtendedButtonVariant }
 
-export const Button = forwardRef((props: ButtonProps, ref: Ref<HTMLButtonElement>) => {
-  return (
-    <MantineButton
-      ref={ref}
-      {...props}
-      classNames={classesNames}
-    />
-  )
-})
+export const Button = createPolymorphicComponent<'button', ButtonProps>(
+  forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
+    return (
+      <MantineButton
+        ref={ref}
+        {...props}
+        classNames={classesNames}
+      />
+    )
+  })
+)
