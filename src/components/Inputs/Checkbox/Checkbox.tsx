9062ec36f208493d7ac69e8e98e607ece443f531
az33zy <10743275+az33zy@users.noreply.github.com>
2025-05-27 10:41:58 +0500

    fix: improve checkbox styling

--- src/components/Inputs/Checkbox/Checkbox.tsx
+++ src/components/Inputs/Checkbox/Checkbox.tsx
@@ -1,17 +1,22 @@
 import { Checkbox as MantineCheckbox } from '@mantine/core'
 import textClassNames from '../../../styles/typography.module.css'
 import classNames from './Checkbox.module.css'
-import { CAPTION_L_REGULAR_FONT_VARIANT } from '../../../constants/font-variants'
+import { CAPTION_L_MEDIUM_FONT_VARIANT } from '../../../constants/font-variants'
 import clsx from 'clsx'
 
 // ----------------------------------------------------------------------
 
-const inputClassName = clsx(textClassNames[CAPTION_L_REGULAR_FONT_VARIANT], classNames.label)
+const inputClassName = clsx(textClassNames[CAPTION_L_MEDIUM_FONT_VARIANT], classNames.label)
 
 // ----------------------------------------------------------------------
 
 export const Checkbox = MantineCheckbox.withProps({
-  radius: 'xs',
-  size: 'xs',
-  classNames: { label: inputClassName, root: classNames.root },
+  size: 'sm',
+  classNames: {
+    label: inputClassName,
+    root: classNames.root,
+    inner: classNames.inner,
+    input: classNames.input,
+    icon: classNames.icon,
+  },
 })
