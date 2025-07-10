1e76597a3d9da895ed99e31742aa6222a2856c21
Aziz Kasymov <a.kasymov@kvell.ru>
2025-07-10 12:48:36 +0700

    PSP-2017: add body-s-semibold font variant

--- src/constants/font-variants.ts
+++ src/constants/font-variants.ts
@@ -8,6 +8,7 @@ export const BODY_M_BOLD_FONT_VARIANT = 'body-m-bold' as const
 export const BODY_L_BOLD_FONT_VARIANT = 'body-l-bold' as const
 export const BODY_S_MEDIUM_FONT_VARIANT = 'body-s-medium' as const
 export const BODY_S_REGULAR_FONT_VARIANT = 'body-s-regular' as const
+export const BODY_S_SEMIBOLD_FONT_VARIANT = 'body-s-semibold' as const
 
 //caption
 export const CAPTION_L_MEDIUM_FONT_VARIANT = 'caption-l-medium' as const
@@ -26,6 +27,7 @@ export const FontVariants = [
   BODY_M_BOLD_FONT_VARIANT,
   BODY_S_MEDIUM_FONT_VARIANT,
   BODY_S_REGULAR_FONT_VARIANT,
+  BODY_S_SEMIBOLD_FONT_VARIANT,
   //caption
   CAPTION_L_MEDIUM_FONT_VARIANT,
   CAPTION_L_REGULAR_FONT_VARIANT,
