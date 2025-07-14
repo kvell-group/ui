fcc50858b47a1aaddeb209f4e589ebfca5e3af69
Aziz Kasymov <a.kasymov@kvell.ru>
2025-07-14 19:18:02 +0700

    PSP-2017: improve styles

--- src/components/Inputs/Input/Input.tsx
+++ src/components/Inputs/Input/Input.tsx
@@ -1,22 +1,10 @@
-import '@mantine/core/styles/Input.layer.css'
 import { TextInput } from '@mantine/core'
+import '@mantine/core/styles/Input.layer.css'
 import classNames from './Input.module.css'
-import textClassNames from '../../../styles/typography.module.css'
-import clsx from 'clsx'
-import { CAPTION_L_REGULAR_FONT_VARIANT } from '../../../constants/font-variants'
-
-// ----------------------------------------------------------------------
-
-const { input, error, ...restClassNames } = classNames
-
-const captionLRegular = textClassNames[CAPTION_L_REGULAR_FONT_VARIANT]
-
-const inputClassNames = clsx(input, captionLRegular)
-const errorClassNames = clsx(error, captionLRegular)
 
 // ----------------------------------------------------------------------
 
 export const Input = TextInput.withProps({
-  classNames: { ...restClassNames, input: inputClassNames, error: errorClassNames },
+  classNames,
   inputWrapperOrder: ['label', 'input', 'error', 'description'],
 })
--- src/components/Inputs/PasswordInput/PasswordInput.tsx
+++ src/components/Inputs/PasswordInput/PasswordInput.tsx
@@ -1,26 +1,12 @@
 import { PasswordInput as PasswordInputBase } from '@mantine/core'
 import classNames from '../Input/Input.module.css'
 import sectionClassNames from './PasswordInput.module.css'
-import textClassNames from '../../../styles/typography.module.css'
-import clsx from 'clsx'
-import { CAPTION_L_REGULAR_FONT_VARIANT } from '../../../constants/font-variants'
-
-// ----------------------------------------------------------------------
-
-const { input, error, ...restClassNames } = classNames
-
-const captionLRegular = textClassNames[CAPTION_L_REGULAR_FONT_VARIANT]
-
-const inputClassNames = clsx(input, captionLRegular)
-const errorClassNames = clsx(error, captionLRegular)
 
 // ----------------------------------------------------------------------
 
 export const PasswordInput = PasswordInputBase.withProps({
   classNames: {
-    ...restClassNames,
-    input: inputClassNames,
-    error: errorClassNames,
+    ...classNames,
     section: sectionClassNames.section,
   },
   autoComplete: 'current-password',
