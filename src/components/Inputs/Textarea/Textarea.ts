0c4779603afb53c98344dbaf040d29bacea188fd
Aziz Kasymov <a.kasymov@kvell.ru>
2025-08-14 12:56:02 +0500

    PSP-2017: improve Textarea styles

--- src/components/Inputs/Textarea/Textarea.ts
+++ src/components/Inputs/Textarea/Textarea.ts
@@ -1,9 +1,15 @@
 import { Textarea as MantineTextarea } from '@mantine/core'
+import clsx from 'clsx'
 import classNames from '../Input/Input.module.css'
+import textareaClassNames from './Textarea.module.css'
+
+// ----------------------------------------------------------------------
+
+const inputClassName = clsx(textareaClassNames.input, classNames.input)
 
 // ----------------------------------------------------------------------
 
 export const Textarea = MantineTextarea.withProps({
-  classNames,
+  classNames: { ...classNames, input: inputClassName },
   inputWrapperOrder: ['label', 'input', 'error', 'description'],
 })
