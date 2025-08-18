102245a1ddf8c9fd9d8aef06e3c2d13e451341b0
Aziz Kasymov <a.kasymov@kvell.ru>
2025-08-18 17:51:18 +0500

    PSP-2180: add PinInput wrapper

--- src/components/Inputs/PinInput/PinInput.tsx
+++ src/components/Inputs/PinInput/PinInput.tsx
@@ -1,4 +1,4 @@
-import { PinInput as PinInputComponent } from '@mantine/core'
+import { Input, PinInput as PinInputComponent } from '@mantine/core'
 import type { PinInputProps as BasePinInputProps } from '@mantine/core'
 
 import classes from './PinInput.module.css'
@@ -20,20 +20,20 @@ type PinInputProps = Omit<BasePinInputProps, 'error'> & {
 export const PinInput = forwardRef<HTMLInputElement, PinInputProps>((props, ref) => {
   const { error, size = 'lg', gap = 4, radius = 'xl', ...restProps } = props
 
-  const isError = Boolean(error)
-
   return (
-    <>
+    <Input.Wrapper
+      error={error}
+      classNames={{ error: inputBaseClasses['error-message'] }}
+    >
       <PinInputComponent
         ref={ref}
         {...restProps}
         radius={radius}
         gap={gap}
         size={size}
-        error={isError}
+        error={Boolean(error)}
         classNames={{ ...classes, input: inputClassName }}
       />
-      {isError && <span className={inputBaseClasses['error-message']}>{error}</span>}
-    </>
+    </Input.Wrapper>
   )
 })
