cf45e1296f6e9d7988effbf765b27830afcb2cd7
Aziz Kasymov <a.kasymov@kvell.ru>
2025-08-19 18:21:01 +0500

    PSP-2017: improve password input

--- src/components/Inputs/PasswordInput/PasswordInput.tsx
+++ src/components/Inputs/PasswordInput/PasswordInput.tsx
@@ -1,14 +1,19 @@
 import { PasswordInput as PasswordInputBase } from '@mantine/core'
 import classNames from '../Input/Input.module.css'
 import sectionClassNames from './PasswordInput.module.css'
+import { RiEyeLine, RiEyeOffLine } from '@remixicon/react'
+import clsx from 'clsx'
 
 // ----------------------------------------------------------------------
 
 export const PasswordInput = PasswordInputBase.withProps({
   classNames: {
     ...classNames,
-    section: sectionClassNames.section,
+    root: clsx(classNames.root, sectionClassNames.root),
+    section: clsx(classNames.section, sectionClassNames.section),
   },
   autoComplete: 'current-password',
   inputWrapperOrder: ['label', 'input', 'error', 'description'],
+  visibilityToggleIcon: ({ reveal }) =>
+    reveal ? <RiEyeOffLine size={20} /> : <RiEyeLine size={20} />,
 })
