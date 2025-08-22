0bd0ffad09e9866f2aa4f5091a9687d4b18f0ae0
Aziz Kasymov <a.kasymov@kvell.ru>
2025-08-22 20:31:05 +0500

    PSP-2017: add link button variant

--- src/components/Button/constants.ts
+++ src/components/Button/constants.ts
@@ -2,12 +2,14 @@ export const PRIMARY_BUTTON_VARIANT = 'primary' as const
 export const SECONDARY_BUTTON_VARIANT = 'secondary' as const
 export const TERTIARY_BUTTON_VARIANT = 'tertiary' as const
 export const GHOST_BUTTON_VARIANT = 'ghost' as const
+export const LINK_BUTTON_VARIANT = 'link' as const
 
 export const ButtonVariants = [
   PRIMARY_BUTTON_VARIANT,
   SECONDARY_BUTTON_VARIANT,
   TERTIARY_BUTTON_VARIANT,
   GHOST_BUTTON_VARIANT,
+  LINK_BUTTON_VARIANT,
 ]
 
 // ----------------------------------------------------------------------
