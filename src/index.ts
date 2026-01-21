19b8aba7fc2f6881dcc621cc197af7b22c74638b
Marat <m.mukhamedshin@kvell.ru>
2026-01-21 19:36:44 +0500

    fix: export charts

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ export { Carousel } from './components/Carousel'
 export { Card } from './components/Card'
 export * from './components/ActionIcon'
 export * from './components/Pagination'
-export * from './components/Charts'
+export { BarChart, LineChart } from './components/Charts'
 
 // core-components ------------------------------------------------------
 
